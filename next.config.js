156499a66b75ac19808963e3f90bcef9a9837b34
ryanbakkerNZ <ryan@ryanbakker.nz>
2023-05-27 22:33:42 +1200

    UI and columns functions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["images.ryanbakker.nz"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
